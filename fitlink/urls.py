b443b637175bbecf5799571cf518ea6817875497
Nuno Vidal <nunovidal00@gmail.com>
2022-06-02 15:22:09 +0100

    big diff 2

--- fitlink/urls.py
+++ fitlink/urls.py
@@ -3,5 +3,14 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index,name="index")
+    path('',views.index,name="index"),
+    path('sign-in',views.signin,name="signin"),
+    path('sign-up',views.signup,name="signup"),
+    path('profile',views.profile,name="profile"),
+    path('notifications',views.notifications,name="notifications"),
+    path('virtual-reality',views.vr,name="virtual-reality"),
+    path('billing',views.billing,name="billing"),
+    path('tables',views.tables,name="tables"),
+    path('dashboard',views.dashboard,name="dashboard"),
+    path('rtl',views.rtl,name="rtl"),
 ] 
--- fitlink/views.py
+++ fitlink/views.py
@@ -4,3 +4,30 @@ from .models import Planos
 
 def index(request):
     return render(request,'index.html',{})
+
+def signin(request):
+    return render(request,'sign-in.html',{})
+
+def signup(request):
+    return render(request,'sign-up.html',{})
+
+def profile(request):
+    return render(request,'profile.html',{})
+
+def notifications(request):
+    return render(request,'notifications.html',{})
+
+def vr(request):
+    return render(request,'virtual-reality.html',{})
+
+def billing(request):
+    return render(request,'billing.html',{})
+
+def tables(request):
+    return render(request,'tables.html',{})
+
+def dashboard(request):
+    return render(request,'dashboard.html',{})
+
+def rtl(request):
+    return render(request,'rtl.html',{})
