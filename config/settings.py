b92b63d9b70640aa23e03a0e10fcb4087a314550
Nuno Vidal <nunovidal00@gmail.com>
2022-06-02 14:02:49 +0100

    settings2

--- config/settings.py
+++ config/settings.py
@@ -117,9 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATICFILES_DIRS = [
-BASE_DIR / "static",
-]
+
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "static"
 
