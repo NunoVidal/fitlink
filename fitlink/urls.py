bbf44ab044c7fcab2d13924a78fd3e47c85bed1f
unknown <nunovidal00@gmail.com>
2022-06-07 19:29:37 +0100

    default page

--- fitlink/urls.py
+++ fitlink/urls.py
@@ -3,7 +3,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index,name="index"),
+    path('',views.profile,name="index"),
     path('sign-in',views.signin,name="signin"),
     path('sign-up',views.signup,name="signup"),
     path('profile',views.profile,name="profile"),
